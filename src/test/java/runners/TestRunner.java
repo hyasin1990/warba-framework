d7d04b8c3fc704db44e2e3c235f831521b4ee189
husam.yaseen <husam.yaseen@warbabank.com>
2020-06-20 13:15:26 +0300

    runner

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -8,7 +8,6 @@ import lombok.extern.log4j.Log4j2;
 // tags = {"@CreateHassala"
 //   +" or @RemoveSaveAs"
 
-// git hub edit
 @CucumberOptions(
         features = "src/test/java/features",
         tags = {
